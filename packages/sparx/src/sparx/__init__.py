445def3a664af570440659c3fe061cfca020976a
Anand Balakrishnan <anandbala1597@gmail.com>
2026-01-20 15:13:41 -0600

    remove `sparx` from this repository

--- packages/sparx/src/sparx/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Implementations of sparse multidimensional arrays in JAX
-
-This project is a port of [sparse](https://sparse.pydata.org/en/stable/) that works
-using JAX arrays and other primitives.
-"""
-
-import importlib.metadata
-
-__version__ = importlib.metadata.version("sparx")
--- packages/sparx/src/sparx/_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .common import equivalent as equivalent
-from .common import normalize_axis as normalize_axis
-from .slicing import normalize_index as normalize_index
