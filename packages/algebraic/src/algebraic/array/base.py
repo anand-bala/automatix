algebraic: fix issues with torch gradients not being propagated correctly

packages/algebraic/src/algebraic/array/base.py
@@ -50,8 +50,13 @@ class AlgebraicArray:
 
     def _wrap(self, data: Array | Number) -> Self:
         """Create a new instance with the given data, preserving all other attributes."""
-        array_ns = array_api_compat.array_namespace(self.data)
-        data = typing.cast(Array, array_ns.asarray(data))
+        if array_api_compat.is_torch_array(data):
+            import torch
+
+            data = typing.cast(Array, torch.as_tensor(data))
+        else:
+            array_ns = array_api_compat.array_namespace(self.data)
+            data = typing.cast(Array, array_ns.asarray(data))
         return self._replace_attr("data", data)
 
     def __add__(self, other: Self | Scalar) -> Self:

packages/algebraic/src/algebraic/ops/_creation.py
@@ -57,7 +57,9 @@ def array(
     elif backend == Backend.TORCH:
         import torch
 
-        return AlgebraicArray(torch.asarray(data, dtype=torch.float32), semiring)
+        if isinstance(data, torch.Tensor):
+            return AlgebraicArray(data.to(dtype=torch.float32), semiring)
+        return AlgebraicArray(torch.tensor(data, dtype=torch.float32), semiring)
     elif backend == Backend.NUMPY:
         import numpy as np
 